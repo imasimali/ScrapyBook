c5ef239ca1312385f71132b8058387f2f78df6ff
Asim Ali <m.asimali69@gmail.com>
2022-01-26 12:32:24 +0000

    eng lang

--- twitter.py
+++ twitter.py
@@ -9,7 +9,7 @@ maxTweets = 50
 tweets_list = []
 
 # Using TwitterSearchScraper to scrape data and append tweets to list
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('aapl since:2022-01-25 until:2022-01-26').get_items()):
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('aapl since:2022-01-25 until:2022-01-26 lang:en').get_items()):
     if i>maxTweets:
         break
     tweets_list.append([tweet.date, tweet.content])
